980ba407f3363aaff1cdae11676308fad3125824
Juan Pablo Carabali <champol200@gmail.com>
2021-10-02 03:47:08 -0500

    Finalizado

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash,session,jsonify,render_template
+from flask import Flask, flash,session,jsonify,render_template,redirect,url_for
 import requests
 import json
 app = Flask(__name__)
@@ -15,12 +15,19 @@ def index():
     response_json = datos()
     return render_template('index.html',datos=response_json["data"])
 
-@app.route('/crear_datos',methods=['GET'])
+@app.route('/crear_datos',methods=['GET','POST'])
 def crear_datos():
-    url = "http://api-fastapi/registros_falsos"
+    url = "http://api-fastapi/crear_registros"
     response = requests.get(url)
     response_json = json.loads(response.text)
-    return render_template('index.html',datos=response_json["data"])
+    return redirect(url_for('index'))
+
+@app.route('/eliminar_datos',methods=['GET','POST'])
+def eliminar_datos():
+    url = "http://api-fastapi/eliminar_registros"
+    response = requests.get(url)
+    response_json = json.loads(response.text)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=80, debug=True)
